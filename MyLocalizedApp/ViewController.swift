cd86268c22a36cfc442df8213f50886b900bc16e
Yahya Saddiq <yahya.saddiq1@vodafone.com>
2020-06-03 11:06:55 +0200

    Initial Commit

---
+++ MyLocalizedApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MyLocalizedApp
+//
+//  Created by Yahya Saddiq on 6/3/20.
+//  Copyright © 2020 Yahya Saddiq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
